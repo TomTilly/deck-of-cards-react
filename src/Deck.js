545fa6d8a037ae0276ee7c60c234229bb61d1275
Tom Tillistrand <tomtillistrand@gmail.com>
2020-12-28 15:38:33 -0500

    Add asynchronous request for deck of cards and store ID

--- src/Deck.js
+++ src/Deck.js
@@ -1,30 +1,65 @@
 import React, { Component } from 'react';
 import Card from './Card';
 
+function checkStatusAndParse(response) {
+  if (!response.ok)
+    throw new Error(
+      `Error fetching resource. Response Status Code: ${response.status}`
+    );
+
+  return response.json();
+}
+
 class Deck extends Component {
   static defaultProps = {
-    shuffleApi: 'https://deckofcardsapi.com/api/deck/new/shuffle',
+    newDeckUrl: 'https://deckofcardsapi.com/api/deck/new/shuffle/',
   };
 
   constructor(props) {
     super(props);
     this.state = {
       isDeckEmpty: false,
+      hasError: false,
+      deckID: '',
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const { newDeckUrl } = this.props;
+    fetch(newDeckUrl)
+      .then(checkStatusAndParse)
+      .then(({ deck_id: deckID }) => {
+        this.setState({ deckID });
+      })
+      .catch((err) => {
+        this.setState({ hasError: true });
+        console.log(err);
+      });
+  }
 
   render() {
+    const { isDeckEmpty, hasError } = this.state;
     const generateNewDeckHtml = (
-      <button className="Deck-newDeckBtn" type="button">
-        Get a new deck!
-      </button>
+      <div>
+        <p>Deck is empty!</p>
+        <button className="Deck-newDeckBtn" type="button">
+          Get a new deck!
+        </button>
+      </div>
     );
-    const { isDeckEmpty } = this.state;
+    const errorHtml = (
+      <p className="error">
+        There was a problem communicating with the
+        <a href="http://deckofcardsapi.com/" target="noopener noreferrer">
+          Deck of Cards API.
+        </a>
+        &nbsp;Please try again in a minute.
+      </p>
+    );
     return (
       <section className="Deck">
         <h1>Dealer</h1>
+        {hasError && errorHtml}
         <button className="Deck-drawBtn" type="button">
           Draw a card
         </button>
